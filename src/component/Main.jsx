9b53f1e7874e62d3f73bebb35885a3d50bb146d1
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-05-18 17:08:04 +0600

    footer is added

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Home/Navbar';
+import Footer from './Pages/Footer';
 
 const Main = () => {
     return (
         <div className='px-8'>
             <Navbar></Navbar>
            <Outlet></Outlet>
+           <Footer></Footer>
         </div>
     );
 };
